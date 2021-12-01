7cd23e7fe98c970a5b5bd52e219d1daf06df8380
Benjamin HOARAU <ben430hoarau@gmail.com>
2021-12-01 19:29:44 +0400

    Création du model_brand

---
+++ app/Models/MODEL/Model_brand.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Models\MODEL;
+
+use App\Models\DAO\Dao_brand;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Model_brand extends Model
+{
+    function createIt($brand)
+    {
+        try {
+            DB::insert(
+                'INSERT INTO production.brands (brand_name) values (?)',
+                [
+                    $brand->getBrandName(),
+                ]
+            );
+            $result = true;
+        } catch (\Exception $e) {
+            $result = $e;
+        }
+        return $result;
+    }
+
+    function updateIt($brand)
+    {
+        try {
+            DB::update(
+                'UPDATE production.products set product_name = ?, model_year = ?, list_price = ?, brand_id = ?, category_id = ? WHERE product_id = ?',
+                [
+                    $brand->getBrandName(),
+                    $brand->getBrandId()
+                ]
+            );
+            $result = true;
+        } catch (\Exception $e) {
+            $result = $e;
+        }
+
+        return $result;
+    }
+
+    function findAll()
+    {
+        $query = DB::select(
+            'SELECT brand_id, brand_name FROM production.brands',
+        );
+
+        $results = [];
+
+        foreach ($query as $value) {
+            $brand = new Dao_brand(
+                $value->brand_id,
+                $value->brand_name
+            );
+            array_push($results, $brand);
+        }
+
+        return $results;
+    }
+
+    function findIt($id)
+    {
+        try {
+            $result = DB::select(
+                'SELECT brand_id, brand_name FROM production.brands WHERE brand_id = ?',
+                [$id]
+            );
+        } catch (\Exception $e) {
+            $result = $e;
+        }
+
+        return $result;
+    }
+
+    function dropIt($id)
+    {
+        try {
+            DB::delete(
+                'DELETE FROM production.brands WHERE brand_id = ?',
+                [$id]
+            );
+            $result = true;
+        } catch (\Exception $e) {
+            $result = $e;
+        }
+
+        return $result;
+    }
+}
